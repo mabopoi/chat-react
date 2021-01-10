778ee029527af80a6b11d23c25e29fb300d3600a
unknown <mabopoi@gmail.com>
2021-01-10 02:42:24 -0300

    Continuing Home's main section

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,29 +4,33 @@ import './index.css';
 type propsTypes = {
   handleSubmit: (e: FormEvent) => void;
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  signIn: boolean;
+  signUp: boolean;
 };
 
-const Form = ({ handleSubmit, handleChange, signIn }: propsTypes) => {
+const Form = ({ handleSubmit, handleChange, signUp }: propsTypes) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <span className='form__input'>Email</span>
-      <input className='form__text' type='text' onChange={handleChange} />
-      {signIn && (
+    <form className='form' onSubmit={handleSubmit}>
+      <span className='form__text'>Email</span>
+      <input className='form__input' type='text' onChange={handleChange} />
+      {signUp && (
         <>
-          <span className='form__input'>Name</span>
-          <input className='form__text' type='text' onChange={handleChange} />
+          <span className='form__text'>Name</span>
+          <input className='form__input' type='text' onChange={handleChange} />
         </>
       )}
-      <span className='form__input'>Password</span>
-      <input className='form__text' type='text' onChange={handleChange} />
-      {signIn && (
+      <span className='form__text'>Password</span>
+      <input className='form__input' type='text' onChange={handleChange} />
+      {signUp && (
         <>
-          <span className='form__input'>Confirm password</span>
-          <input className='form__text' type='text' onChange={handleChange} />
+          <span className='form__text'>Confirm password</span>
+          <input className='form__input' type='text' onChange={handleChange} />
         </>
       )}
-      <button>Go!</button>
+      <div className='form__btnContainer'>
+        <button type='submit' className='form__btn'>
+          Go!
+        </button>
+      </div>
     </form>
   );
 };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,43 @@
+import { useState } from 'react';
+import Form from '../Form';
 import './index.css';
 
 const Main = () => {
+  const [signUp, setSignUp] = useState(true);
+  const handleChange = () => {};
+  const handleSignUpSubmit = () => {};
+  const handleLogInSubmit = () => {};
+
   return (
     <main className='main'>
       <div className='main__chart'>
         <ul className='main__list'>
-          <div className='main__listItem'>
+          <div className='main__listItem' onClick={() => setSignUp(true)}>
             <li>Sign up</li>
           </div>
-          <div className='main__listItem'>
+          <div className='main__listItem' onClick={() => setSignUp(false)}>
             <li>Log in</li>
           </div>
         </ul>
+        {signUp ? (
+          <div className='main__chartContent'>
+            <h2 className='main__chartTitle'>Sign up!</h2>
+            <Form
+              handleSubmit={handleSignUpSubmit}
+              handleChange={handleChange}
+              signUp={true}
+            />
+          </div>
+        ) : (
+          <div className='main__chartContent'>
+            <h2 className='main__chartTitle'>Log in!</h2>
+            <Form
+              handleSubmit={handleLogInSubmit}
+              handleChange={handleChange}
+              signUp={false}
+            />
+          </div>
+        )}
       </div>
     </main>
   );
